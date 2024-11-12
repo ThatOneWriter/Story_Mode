42a671dc7368fc5ef087e8165fcf8c4c71dcb248
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2024-11-12 10:51:32 -0500

    Student.java
    11/12/24 - I further developed this object file more and created my main constructors.

--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,41 @@
 public class Student {
-    private String aspirations;
-    private int grades;
+
+    private String name;
+
+    private int age;
+
+    private String year;
+
     private String descriptions;
+
+    private User student;
+
+    private User student1;
 
     public Student(String name, int age, String year, String descriptions){
 
+        this.name = name;
+        this.age = age;
+        this.year = year;
+        this.descriptions = descriptions;
+
     }
 
+    public String getDescriptions(){
+
+        int grades;
+        String aspirations;
+
+        return descriptions;
+    }
     public void studentSimulation() {
 
+        student.simulation();
+        student1.simulation();
+
+
     }
+
+
 
 }
