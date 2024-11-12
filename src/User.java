88a82589b2a41b1cf083de392683cc634d563ef0
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2024-11-12 10:56:35 -0500

    User.java
    11/12/24 - I set the main constructors up, I mostly need to finish getLikableness and incorporate it into the main story as well as add to simulation.

--- src/User.java
+++ src/User.java
@@ -1,22 +1,74 @@
 public class User {
+
     private String name;
+
     private int likableness;
+
     private String simulation;
 
-    public User(){
+    public User(String name, int likableness){
+
+      this.name = name;
+      this.likableness = likableness;
 
     }
 
-
     public String getName(){
+
         return name;
+
+    }
+
+    public int getLikableness(int likableness){
+
+        int anti = 1;
+        likableness = 5;
+        while (true) {
+
+            return likableness - anti;
+
+        }
+
     }
 
     public void simulation() {
 
+        int simulate = 1 + (int) (Math.random() * 8);
+
+        if (simulate == 1) {
+            System.out.println();
+        }
+        if (simulate == 2) {
+            System.out.println();
+        }
+        if (simulate == 3) {
+            System.out.println();
+        }
+        if (simulate == 4) {
+            System.out.println();
+        }
+        if (simulate == 5) {
+            System.out.println();
+        }
+        if (simulate == 6) {
+            System.out.println();
+        }
+        if (simulate == 7) {
+            System.out.println();
+        }
+        if (simulate == 8) {
+            System.out.println();
+        }
+
     }
 
+    public void endGame() {
 
+        if (likableness == 0) {
+            System.out.println("Sorry, but you have lost all of your credibility. Better luck next time.");
+        }
+
+    }
 
 
 }
