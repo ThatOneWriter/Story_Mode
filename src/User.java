5958e43df235d1d9421dbb7c3c0e1fe89a8d011d
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2024-11-14 10:57:49 -0500

    User.java
    11/14/24 - Almost fully completed user class with Javadocs

--- src/User.java
+++ src/User.java
@@ -1,3 +1,7 @@
+/**
+ * The User class represents the user. A user is the one playing and interacting with the game.
+ * They are assigned a name, a likableness score, and a simulation ability
+ */
 public class User {
 
     private String name;
@@ -6,6 +10,14 @@ public class User {
 
     private String simulation;
 
+    /**
+     * Constructor for the User class. This creates an instance of the user given the
+     * parameters mentioned below.
+     *
+     * @param name represents the name of the user. They are given one by random choice
+     * @param likableness represents the users likability score.
+     *
+     */
     public User(String name, int likableness){
 
       this.name = name;
@@ -13,23 +25,65 @@ public class User {
 
     }
 
+    /**
+     * getName method for the User class. This method will return a String
+     * which displays the name of the user.
+     *
+     * @return returns the randomly generated name of the user.
+     *
+     */
     public String getName(){
-       name = "Azz";
+        int randomName = 1 + (int) (Math.random() * 3);
 
-        return name;
-
-    }
-
-    public int likablenessDecreases(){
-
-        int anti = 1;
-        while (true) {
-
-            return likableness - anti;
-
+        if (randomName == 1) {
+            name = "Butt-cheeks";
+            return name;
         }
-
+        if (randomName == 2) {
+            name = "Azz";
+            return name;
+        }
+        if (randomName == 3) {
+            name = "-insert main character-";
+            return name;
+        }
+        return name;
     }
+
+    /**
+     * the likablenessDecreases method for the User class.
+     * This method will return an int displaying the users likableness decreasing.
+     *
+     * @return returns the users likableness - 1
+     *
+     */
+    public int likablenessDecreases(){
+            int thatsnotNice = 1;
+            return likableness - thatsnotNice;
+    }
+
+    /**
+     * the likablenessIncreases method for the User class.
+     * This method will return an int displaying the users likableness increasing
+     * or staying the same if it is already 5.
+     *
+     * @return returns the users likableness + 1
+     *
+     */
+    public int likablenessIncreases(){
+
+        if (likableness == 5) {
+            return 5;
+        }
+        return likableness++;
+    }
+
+    /**
+     * The simulation method will store fighting based simulations for the user to
+     * randomly call upon.
+     *
+     * @return returns a String representing a randomly picked simulation.
+     */
 
     public String simulation() {
 
@@ -60,6 +114,9 @@ public class User {
         }
 
     }
+
+
+}
 
 
 }
