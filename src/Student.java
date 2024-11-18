f740215ef396387b960677554b48c928cc0b2bab
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2024-11-18 10:55:50 -0500

    Student.java
    11/18/24 Finished the description method, just need to add text.

--- src/Student.java
+++ src/Student.java
@@ -6,8 +6,6 @@ public class Student {
 
     private String year;
 
-    private String descriptions;
-
     private User student;
 
     private User student1;
@@ -24,15 +22,43 @@ public class Student {
     /**
      * The getDescriptions method will
      *
-     * @param grades an integer representing
-     * @param aspirations a String representing
+     * @param grades an integer representing the students previous average.
+     * @param aspirations a String representing the students' mindset.
      * @return a String representing a formatted paragraph showcasing a detailed description of the student.
      */
     public String getDescriptions(int grades, String aspirations){
 
+        int aspiration_randomizer = 1 + (int) (Math.random() * 5);
+
+        if (aspiration_randomizer == 1) {
+            aspirations = "1";
+            return aspirations;
+        }
+        if (aspiration_randomizer == 2) {
+            aspirations = "2";
+            return aspirations;
+        }
+        if (aspiration_randomizer == 3) {
+            aspirations = "3";
+            return aspirations;
+        }
+        if (aspiration_randomizer == 4) {
+            aspirations = "4";
+            return aspirations;
+        }
+        if (aspiration_randomizer == 5) {
+            aspirations = "5";
+            return aspirations;
+        }
+
+        String descriptions = "Students Name: " + name + "\n";
+        descriptions += "Age and Year: " + age + ", Grade " + year + "\n";
+        descriptions += "Previous Years' Overall Average: " + grades + "\n";
+        descriptions += "Aspirations: " + aspirations;
 
         return descriptions;
     }
+
     public void studentSimulation() {
 
         student.simulation();
@@ -40,4 +66,6 @@ public class Student {
 
 
     }
+    
 
+}
